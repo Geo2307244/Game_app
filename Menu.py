77bc8d2ba78cbd628945a12cfbf125174a797924
Geo2307244 <163450211+Geo2307244@users.noreply.github.com>
2024-04-03 22:50:15 -0500

    Game_demo.py
    Added the Short Welcome

--- Menu.py
+++ Menu.py
@@ -1,9 +1,10 @@
 def play_Riddle_it_Roulette():
-    print("Welcome to Riddle It Roulette")
-    print("Let's Begin. You will be given a phrase to guess the word I have in mind.")
-    print("Get it right, you move on. Get it wrong, you lose a life!")
-    print("Lose 3 times, it's game over.\n")
-
+    print()
+    print("\033[1;32m" + "Welcome to Riddle It Roulette" + "\033[0m") 
+    print("\033[1;32m" + "Let's Begin. You will be given a phrase to guess the word I have in mind." + "\033[0m")
+    print("\033[1;32m" + "Get it right, you move on. Get it wrong, you lose a life!" + "\033[0m")
+    print("\033[1;32m" + "Lose 3 times, it's game over." + "\033[0m")
+    print()
     import random
 
     words = {
@@ -51,28 +52,28 @@ def play_Riddle_it_Roulette():
         while True:
             word_display = display_word(word_to_guess, guessed_word)
             print("\n" + word_display)
-            print("Hint: ", hint)
+            print("\033[1;32m" + "Hint: " + hint + "\033[0m")  
 
             if word_display.lower() == word_to_guess:
-                print("\nThats Correct! {} Was The Word".format(word_to_guess))
+                print("\n\033[1;33m" + "Thats Correct! {} Was The Word".format(word_to_guess) + "\033[0m") 
                 score += 1
-                print("congrats\n")
+                print("\033[1;32;40m" + "congrats\n" + "\033[0m")  
                 break
 
             if attempts >= life:
-                print("\nYou Ran Out of Lives!, The Word Was {}\n".format(word_to_guess))
+                print("\n\033[1;31m" + "You Ran Out of Lives!, The Word Was {}\n".format(word_to_guess) + "\033[0m")  
                 break
 
             guess = input("Enter The Correct Word To Proceed: ").lower()
 
             if guess == word_to_guess:
-                print("\nThats Correct! {} Was The Word".format(word_to_guess))
+                print("\n\033[1;33m" + "Thats Correct! {} Was The Word".format(word_to_guess) + "\033[0m")  
                 score += 1
-                print("congrats\n")
+                print("\033[1;32m" + "congrats\n" + "\033[0m")  
                 break
             else:
                 attempts += 1
-                print("That's Not Correct", life - attempts, "lives left.\n")
+                print("\033[1;31m" + "That's Not Correct {} lives left.\n".format(life - attempts, attempts) + "\033[0m")  
 
         games += 1
         replay = input("Do you want to play again? (yes/no): ").lower()
@@ -81,14 +82,15 @@ def play_Riddle_it_Roulette():
             print("\nScore: {}".format(score))
             print("Games Played: {}".format(games))
             print("Average Score: {}".format(score / games))
-
-import random
-
-def number_guessing_game():
-    print("Welcome to The Number Hunter!")
-    print("I'm thinking of a number between 1 and 100.")
-    print("You have 10 attempts to guess the number.")
-    print("For each correct guess, you earn 10 points.")
+            
+def play_number_guessing_game():
+    print()
+    print("\033[0;34m" + "Welcome to The Number Hunter!" + "\033[0m")
+    print("\033[0;34m" + "I'm thinking of a number between 1 and 100." + "\033[0m")
+    print("\033[0;34m" + "You have 10 attempts to guess the number." + "\033[0m")
+    print("\033[0;34m" + "For each correct guess, you earn 10 points." + "\033[0m")
+    print()
+    import random
 
     min_num = 1
     max_num = 100
@@ -104,7 +106,7 @@ def number_guessing_game():
         attempts = 0
 
         print(f"\nRound {rounds_played}:")
-        print(f"I'm thinking of a number between {min_num} and {max_num}. You have {max_attempts} attempts.")
+        print("\033[0;34m" + f"I'm thinking of a number between {min_num} and {max_num}. You have {max_attempts} attempts." + "\033[0m")
 
         while attempts < max_attempts:
             try:
@@ -114,97 +116,103 @@ def number_guessing_game():
                 if guess < min_num or guess > max_num:
                     print(f"Please guess within the range {min_num} and {max_num}.")
                 elif guess < secret_number:
+                    print()
                     print("Too low! Try again.")
+                    print()
                 elif guess > secret_number:
                     print("Too high! Try again.")
+                    print()
                 else:
-                    print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
+                    print()
+                    print("\033[0;33m" + f"Congratulations! You guessed the number {secret_number} in {attempts} attempts." + "\033[0m")
                     points += 10
-                    print(f"You earned {points} points.")
+                    print("\033[0;34m" + f"You earned {points} points." + "\033[0m")
+                    print()
                     total_points += points
                     break
             except ValueError:
                 print("Please enter a valid number.")
+                print()
 
         if attempts >= max_attempts:
-            print(f"Sorry, you've run out of attempts. The correct number was {secret_number}.")
+            print("\033[0;31m" + f"Sorry, you've run out of attempts. The correct number was {secret_number}." + "\033[0m")
 
         play_again = input("Do you want to play again? (yes/no): ").lower()
         if play_again != 'yes':
             print("Thanks for playing!")
             print(f"Total rounds played: {rounds_played}")
             print(f"Total points earned: {total_points}")
+            print()
             break
 
-if __name__ == "__main__":
-    number_guessing_game()
 
+def RockPaperScissorsGame():
+    print()
+    print("\033[3;35m" + "Welcome To Rock, Paper, Scissors" + "\033[0m")
+    print("\033[3;35m" + "The Rules are Quite Simple Just Enter: rock, paper or scissors" + "\033[0m")
+    print()
+    import random
+    
+    choices = ["rock", "paper", "scissors"]
+    player_wins = 0
+    computer_wins = 0
+    draws = 0
 
-# Main code
-print("Welcome To Our App! Select The Game You Want To Play")
-print()
-print("1. Riddle It Roulette")
-print("2. The Number Hunter")
-print("3. Game 3")
-print("4. Game 4")
-print()
-
-game = int(input("Select The Corresponding Number For The Game You Want To Play "))
-
-if game == 1:
-    play_Riddle_it_Roulette()
-elif game == 2:
-    number_guessing_game()
-elif game == 3:
-    print("Game 3")
-elif game == 4:
-    print("Game 4")
-else:
-    print("Invalid option")
-
-import random
-
-class RockPaperScissorsGame:
-    def __init__(self):
-        self.choices = ["rock", "paper", "scissors"]
-        self.reset()
-
-    def play(self, player_choice):
-        player_choice = player_choice.lower()
-        computer_choice = random.choice(self.choices)
-        if player_choice == computer_choice:
-            self.draws += 1
-            return f"Computer chose {computer_choice}. It's a draw!"
-        elif (player_choice == "rock" and computer_choice == "scissors") or \
-             (player_choice == "paper" and computer_choice == "rock") or \
-             (player_choice == "scissors" and computer_choice == "paper"):
-            self.player_wins += 1
-            return f"Computer chose {computer_choice}. You win!"
-        else:
-            self.computer_wins += 1
-            return f"Computer chose {computer_choice}. Computer wins!"
-
-    def reset(self):
-        self.player_wins = 0
-        self.computer_wins = 0
-        self.draws = 0
-
-    def game_stats(self):
-        total_games = self.player_wins + self.computer_wins + self.draws
-        return (f"Total games: {total_games}\n"
-                f"Wins: {self.player_wins}\n"
-                f"Losses: {self.computer_wins}\n"
-                f"Draws: {self.draws}")
-
-if __name__ == "__main__":
-    game = RockPaperScissorsGame()
     while True:
-        player_input = input("Choose rock, paper, or scissors (or 'exit' to quit): ")
-        if player_input.lower() == 'exit':
+        player_input = input("\033[0;35m" + "Choose rock, paper, or scissors (or 'exit' to quit): " + "\033[0m").lower()
+        if player_input == 'exit':
             break
-        result = game.play(player_input)
-        print(result)
+
+        computer_choice = random.choice(choices)
+        if player_input == computer_choice:
+            draws += 1
+            print("\033[0;33m" + f"Computer chose {computer_choice}. It's a draw!" "\033[0m")
+            print()
+        elif (player_input == "rock" and computer_choice == "scissors") or \
+             (player_input == "paper" and computer_choice == "rock") or \
+             (player_input == "scissors" and computer_choice == "paper"):
+            player_wins += 1
+            print("\033[0;32m" + f"Computer chose {computer_choice}. You win!" + "\033[0m")
+            print()
+        elif player_input not in ["rock", "paper", "scissors"]:
+                print("Invalid Input! Please Enter: rock, paper or scissors")
+                print()
+                continue
+        else:
+            computer_wins += 1
+            print("\033[0;31m" + f"Computer chose {computer_choice}. Computer wins!" + "\033[0m")
+            print()
+            
+
+    total_games = player_wins + computer_wins + draws
     print("\nGame Statistics:")
-    print(game.game_stats())
+    print(f"Total games: {total_games}")
+    print(f"Wins: {player_wins}")
+    print(f"Losses: {computer_wins}")
+    print(f"Draws: {draws}")
 
+while True:
+    print("Welcome To Our App! Select The Game You Want To Play")
+    print() # Colour Scheme from (Studytonight.com)
+    print("\033[3;32m" + "1. Riddle It Roulette" + "\033[0m")  
+    print("\033[3;34m" + "2. Number Guessing Game" + "\033[0m")  
+    print("\033[3;35m" + "3. Rock Paper Scissor's Game" + "\033[0m")  
+    print("\033[3;31m" + "4. Game 4" + "\033[0m")  
+    print()
 
+    game = int(input("Select The Corresponding Number For The Game You Want To Play "))
+
+    if game == 1:
+        play_Riddle_it_Roulette()
+    elif game == 2:
+        play_number_guessing_game()
+    elif game == 3:
+        RockPaperScissorsGame()
+    elif game == 4:
+        print("Game 4")
+    else:
+        print("\033[1;31m" + "Invalid option" + "\033[0m")  
+
+    play_again = input("Do you want to play another game? (yes/no): ").lower()
+    if play_again != "yes":
+        break
